9606db485432dad9d265bae5c820574a8a753ebc
yunlew531 <yunlew531@gmail.com>
2021-07-21 06:55:35 +0800

    [EDIT] 更改路徑

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Latte-Cake-options-api/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/PastaHouse-options-api' : '/'
 };
